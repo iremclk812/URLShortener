596508536af715c8ba137ab499d957c1a2dfbcb2
iremclk812 <iremc901@gmail.com>
2025-03-21 10:59:38 +0300

    Update models.py

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db import models
+
+
+class URL(models.Model):
+    long_url = models.URLField("Original URL")
+    short_url = models.CharField("Short URL", max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    attempts = models.PositiveIntegerField("Attempt Count", default=1)
+
+    def __str__(self):
+        return f"{self.short_url} -> {self.long_url} (Attempts: {self.attempts})"
 
 # Create your models here.
